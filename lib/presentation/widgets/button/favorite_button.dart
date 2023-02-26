187ed8dc88350585d2681b12f97ac3385f949183
Nafla Diva Syafia <nafladiva@gmail.com>
2023-02-26 09:39:46 +0700

    feat: add Fluttertoast

--- lib/presentation/widgets/button/favorite_button.dart
+++ lib/presentation/widgets/button/favorite_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:freeyay/domain/entities/entities.dart';
 import 'package:freeyay/injection.dart';
 import 'package:freeyay/presentation/bloc/bloc.dart';
@@ -32,37 +33,47 @@ class _FavoriteButtonState extends State<FavoriteButton> {
   Widget build(BuildContext context) {
     return BlocProvider.value(
       value: detailGameBloc,
-      child: BlocBuilder<DetailGameBloc, DetailGameState>(
-        bloc: detailGameBloc,
-        builder: (context, state) {
-          final isFavorite = state.isFavorite;
+      child: BlocListener<FavoriteBloc, FavoriteState>(
+        bloc: favoriteBloc,
+        listener: (context, state) {
+          if (state is FavoriteSuccess) {
+            Fluttertoast.showToast(msg: state.message ?? '');
+          }
+        },
+        child: BlocBuilder<DetailGameBloc, DetailGameState>(
+          bloc: detailGameBloc,
+          builder: (context, state) {
+            final isFavorite = state.isFavorite;
 
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: InkWell(
-              onTap: () {
-                if (isFavorite) {
-                  favoriteBloc.add(OnRemoveFavorite(widget.game));
-                } else {
-                  favoriteBloc.add(OnAddFavorite(widget.game));
-                }
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () {
+                  if (isFavorite) {
+                    favoriteBloc.add(OnRemoveFavorite(widget.game));
+                  } else {
+                    favoriteBloc.add(OnAddFavorite(widget.game));
+                  }
 
-                detailGameBloc.add(OnCheckFavoriteStatus(widget.game));
-              },
-              child: Container(
-                padding: const EdgeInsets.all(4.0),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(
-                  isFavorite ? Icons.favorite : Icons.favorite_border_outlined,
-                  color: Colors.red,
+                  detailGameBloc.add(OnCheckFavoriteStatus(widget.game));
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(4.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    isFavorite
+                        ? Icons.favorite
+                        : Icons.favorite_border_outlined,
+                    color: Colors.red,
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
